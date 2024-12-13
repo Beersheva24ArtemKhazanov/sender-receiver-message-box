004dbb6bcf716c0c531c906731836b6797cf4a00
Artem Khazanov <tulenka04@gmail.com>
2024-12-13 11:56:15 +0200

    updated branch even-odd-messages

--- src/main/java/telran/producer/consumer/Receiver.java
+++ src/main/java/telran/producer/consumer/Receiver.java
@@ -1,11 +1,13 @@
 package telran.producer.consumer;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Receiver extends Thread {
     private MessageBox messageBox;
 
     public Receiver(MessageBox messageBox) {
         this.messageBox = messageBox;
-        //FIXME update to USER thread with some logic of finishing
         setDaemon(true);
     }
 
@@ -18,11 +20,32 @@ public class Receiver extends Thread {
         while (true) {
             try {
                 String message = messageBox.take();
-                System.out.printf("Thread: %s, message: %s\n", getName(), message);
+                int msgNumber = getNumber(message);
+                int receiverNumber = getNumber(getName());
+                if (isEvenOrOdd(msgNumber, receiverNumber)) {
+                    System.out.printf("Thread: %s, message: %s\n", getName(), message);
+                } else {
+                    messageBox.put(message);
+                    sleep(10);
+                }
             } catch (InterruptedException e) {
 
             }
         }
     }
-    
+
+    private boolean isEvenOrOdd(int msgNumber, int receiverNumber) {
+        return (msgNumber % 2 == 0 && receiverNumber % 2 == 0) ||
+        (msgNumber % 2 != 0 && receiverNumber % 2 != 0); 
+    }
+
+    private int getNumber(String message) {
+        int res = 0;
+        Matcher matcher = Pattern.compile("\\d+").matcher(message);
+        if (matcher.find()) {
+            res = Integer.parseInt(matcher.group());
+        }
+        return res;
+    }
+
 }
